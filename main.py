b4c63f33ecd8dd2da34c0924f8a16833b09dfc87
kocmetehan <metehankoc51@gmail.com>
2024-07-30 08:23:55 +0300

    Platform Manager is created

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 
 from block_manager import BlockManager
 from ball_manager import BallManager
+from platform_manager import PlatformManager
 
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 game_is_on = True
 block_manager = BlockManager()
 ball_manager = BallManager()
+platform_manager = PlatformManager()
 
 block_manager.create_blocks()
 screen.update()
