bef644f2fa178b57cc52bb3e8e92d0f319f39a3b
kocmetehan <metehankoc51@gmail.com>
2024-08-03 15:33:47 +0300

    Game class is created

---
+++ game.py
@@ -0,0 +1,37 @@
+from turtle import Screen
+
+from block_manager import BlockManager
+from ball_manager import BallManager
+from platform_manager import PlatformManager
+
+
+class Game():
+    def __init__(self):
+        self.game_is_on = True
+        self.screen = self.set_screen()
+        self.ball_manager = BallManager()
+        self.block_manager = BlockManager()
+        self.platform_manager = PlatformManager(screen=self.screen)
+
+
+    def set_screen(self):
+        screen = Screen()
+        screen.setup(width=715, height=600)
+        screen.bgcolor("black")
+        screen.tracer(0)
+        return screen
+
+
+    def start_game(self):
+        self.block_manager.create_blocks()
+        self.screen.update()
+        while(self.game_is_on):
+            self.ball_manager.move_ball()
+            block_index = self.ball_manager.is_hit_to_block(self.block_manager.blocks)
+            if block_index >= 0:
+                self.block_manager.delete_block(block_index=block_index)
+                self.ball_manager.change_direction(is_block_hit=True, platform=self.platform_manager.platform)
+            else:
+                self.ball_manager.change_direction(is_block_hit=False, platform=self.platform_manager.platform)
+            self.screen.update()
+        self.screen.exitonclick()
--- main.py
+++ main.py
@@ -1,32 +1,5 @@
-from turtle import Screen
+from game import Game
 
-from block_manager import BlockManager
-from ball_manager import BallManager
-from platform_manager import PlatformManager
-
-
-screen = Screen()
-screen.setup(width=715, height=600)
-screen.bgcolor("black")
-screen.tracer(0)
-
-game_is_on = True
-block_manager = BlockManager()
-ball_manager = BallManager()
-platform_manager = PlatformManager(screen=screen)
-
-block_manager.create_blocks()
-screen.update()
-
-while(game_is_on):
-    ball_manager.move_ball()
-    
-    block_index = ball_manager.is_hit_to_block(block_manager.blocks)
-    if block_index >= 0:
-        block_manager.delete_block(block_index=block_index)
-        ball_manager.change_direction(is_block_hit=True, platform=platform_manager.platform)
-    else:
-        ball_manager.change_direction(is_block_hit=False, platform=platform_manager.platform)
-    screen.update()
-
-screen.exitonclick()
+game = Game()
+while game.game_is_on:
+    game.start_game()
