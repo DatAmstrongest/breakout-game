8efb4744e9962a802232bfc81c1151cc1f51b3f2
kocmetehan <metehankoc51@gmail.com>
2024-08-06 18:00:08 +0300

    Restart the game when lives are out is added

--- game.py
+++ game.py
@@ -43,6 +43,8 @@ class Game():
             block_index = self.ball_manager.is_hit_to_block(self.block_manager.blocks)
             if self.ball_manager.is_hit_vertical_lower_border():
                 self.lower_health()
+                if self.lives == 0:
+                    self.restart_game()
             if block_index >= 0:
                 self.block_manager.delete_block(block_index=block_index)
                 self.ball_manager.change_direction(is_block_hit=True, platform=self.platform_manager.platform)
@@ -55,12 +57,17 @@ class Game():
     def restart_game(self):
         self.block_manager.delete_all_blocks()
         self.ball_manager.delete_ball()
+        self.lives = 3
+        self.set_health()
         self.start_game()
     
     def lower_health(self):
         self.ball_manager.delete_ball()
         self.ball_manager.create_ball()
         self.lives -= 1
+        self.set_health()
+        
+    def set_health(self):
         self.lives_label.clear()
         self.lives_label.write(self.lives, font=FONT)
 
